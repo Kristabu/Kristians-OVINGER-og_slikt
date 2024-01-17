5ea24787ca182b7ee32c5686722aa6600a82a625
Kristabu <Kristabu@stud.ntnu.no>
2024-01-17 15:24:15 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 #include "std_lib_facilities.h"
 
 //------------------------------------------------------------------------------'
+//oppgave a)
 /*
 def maxOfTwo(a, b):
     if a > b:
@@ -18,39 +19,128 @@ def maxOfTwo(a, b):
     return b
 */
 
-int a = 1;
-int b = 0;
 
-
-int main() {
-
-    if (a > b){
-        cout << a << endl;
+int maxof2(int a, int b){
+        if (a > b){
+        cout << "A is greater than B" << endl;
+        return a;
     } else {
-        cout << b << endl;
+        cout << "B is greater than or equal to A" << endl;
+        return b;
     }
-    return 0;
+}
+
+//------------------------------------------------------------------------------
+//oppgave c
+/*
+def fibonacci(n):
+    a = 0
+    b = 1
+    print("Fibonacci numbers:")
+    for x in range(1, n + 1):
+        print(x, b)
+        temp = b
+        b += a
+        a = temp
+    print("----")
+    return b
+*/
+
+int fibonacci(int n){
+    int a = 0;
+    int b = 1;
+    cout << "Fibonacci numbers:" << endl;
+    for (int x = 1; x < n+1; ++x) {
+        cout << x << " " << b << endl;
+        int temp = b;
+        b += a;
+        a = temp;
+    }
+    cout << "----" << endl;
+    return b;
+}
+
+//------------------------------------------------------------------------------
+//oppgave d
+/*
+def squareNumberSum(n):
+    totalSum = 0
+    for i in range(1, n + 1):
+        totalSum += i * i
+        print(i * i)
+    print(totalSum)
+    return totalSum
+*/
+
+int squareNumberSum(int n){
+    int totalSum = 0;
+    for (int i = 1; i < n + 1; ++i){
+        totalSum += (i * i);
+        cout << (i * i) << endl;
+    }
+    cout << totalSum << endl;
+    return totalSum;
+}
+
+//------------------------------------------------------------------------------
+//oppgave e
+/*
+def triangleNumbersBelow(n):
+    acc = 1
+    num = 2
+    print("Triangle numbers below ", n, ":", sep="")
+    while acc < n:
+        print(acc)
+        acc += num
+        num += 1
+    print()
+*/
+void triangleNumbersBelow(int n){
+    int acc = 1;
+    int num = 2;
+    cout << "Triangle numbers below " << n << ":" << endl;
+    while(acc < n){
+        cout << acc << endl; 
+        acc += num;
+        num += 1;
+    }
+    cout << endl;
 }
 
 /*
-// funksjonen 'add' som har to parameter av
-// typen 'int' og returverdi av typen 'int'.
-int add(int a, int b) {
-    // legger sammen verdiene av 'a' og 'b' og returner resultatet
-    return a + b;
-}
-
-int main() {
-    // Skriver 'Hello world!' i konsollen
-    cout << "Hello world!" << endl;
-    // Kaller på 'add' med heltallene 1 og 2 som argumenter.
-    // Returverdien (3) skrives ut i konsollen
-    cout << add(1, 2) << endl;
-    
-    return 0;
-    }
-
+I C++ er det ikke vanlig å ha returverdi for en 'void'-funksjon, siden 'void'
+spesifiserer at funksjonen ikke returnerer noen verdi. Imidlertid kan du vise resultater 
+til skjerm fra en 'void'-funksjon ved å bruke 'std::cout' eller lignende metoder for å 
+skrive til standardutgangen (for eksempel terminalvinduet).
 */
+
+//------------------------------------------------------------------------------
+//oppgave f
+
+
 
 
 //------------------------------------------------------------------------------
+//oppgave b
+/*
+    cout << "" << endl;
+    cout <<  << endl;
+*/
+
+int main() {
+    cout << "Oppgave a)" << endl;
+    cout << maxof2(5, 6) << endl;
+
+    cout << "Oppgave c)" << endl;
+    cout << fibonacci(5) << endl;
+    
+    cout << "Oppgave d)" << endl;
+    cout << squareNumberSum(5) << endl;
+
+    cout << "Oppgave e)" << endl;
+    triangleNumbersBelow(10);
+
+    return 0;
+}
+
+
