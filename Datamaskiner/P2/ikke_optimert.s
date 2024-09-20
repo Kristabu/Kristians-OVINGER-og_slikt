63b66fc919f3f23c12165b5ea2a334d64aeb8d74
KRAB <Kristabu@stud.ntnu.no>
2024-09-20 11:06:13 +0200

    Seems to work
    sqrt implemented

--- Datamaskiner/P2/ikke_optimert.s
+++ Datamaskiner/P2/ikke_optimert.s
@@ -3,29 +3,47 @@
 #output a0 og a1
 
 #initialiser verdier
-#li a0 21 #input
-li s2 2
-li s3 1 #steg i loop
-div a1 a0 s2 #teller a2
+li a0 17 #input, a0 og a1 er outputs
+li a3 0 #sammenligner 
+li s2 2 #divider
+li s3 1 #startverdi loop
+add a4 a4 a0 #mellomlagring av A
+div a5 a4 s2 #teller a5 (A/2)
+li a0 0
+#for loop der i er a5
 
-#for loop der i er a2
-
-Loop:
+Div:
     #Deler
-    #a2 er høyeste resultat
-    div a2 a0 a1
-    mul a3 a2 a1
-    beq a3 a0 Slutt
+    #a5 er høyeste resultat
+    div a7 a4 a5 #div produkt
+    mul a3 a7 a5
+    beq a3 a4 store_value
+    bne a3 a4 Loop
     
+store_value:
+    bne a0 x0 Kvadrattall
+    beq a5 s3 Kvadrattall
+    add a0 a5 x0
+    beq a3 a4 Kvadrattall
+    
+Loop:
     #negativ loop fra A/2 -> 0
-    sub a1 a1 s3
-    beq a1 x0 Slutt
-    bgt a1 x0 Loop
-    
-    #loop funksjonen
-    #addi a3 a3 1 
-    #blt a2 a3 Slutt #i når range
-    #bgt a2 a3 Loop
+    sub a5 a5 s3
+    blt s2 a5 Div
+    bgt s2 a5 Primtall
+    beq a3 a4 Kvadrattall
 
+         
+Kvadrattall:
+    div a6 a4 a5
+    mul a3 a6 a5
+    bne a6 a5 Loop
+    li a1 1
+    bne a1 x0 Primtall
+
+Primtall: 
+    bne a0 x0 Slutt
+    li a0 0
+    li a1 0
 Slutt:
     nop
